9cf98ef3759258b87e3966e0ccc4aebc88bed67e
bolognacarmine-cell <bologna.carmine@gmail.com>
2026-02-13 23:53:32 +0100

    Fix wildcard route

--- server.js
+++ server.js
@@ -44,11 +44,9 @@ app.get("/api/test", (req, res) => {
 // SPA FALLBACK (IMPORTANTISSIMO)
 // -------------------
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
-
 
 
 // -------------------
